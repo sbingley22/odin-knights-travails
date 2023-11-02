ce271e12a8687971a10391b27d99436fd4b77be4
sbingley22 <sbingley22@gmail.com>
2023-11-02 16:40:11 +0000

    added board squares

--- dist/main.js
+++ dist/main.js
@@ -6,13 +6,36 @@ var __webpack_exports__ = {};
 const elements = () => {
   const content = document.querySelector("#content");
   const board = document.querySelector("#board");
+  const squares = [];
 
-  return { content, board };
+  let black = false;
+  for (let i = 0; i < 8; i++) {
+    const temp = [];
+
+    if (black) black = false;
+    else black = true;
+
+    for (let j = 0; j < 8; j++) {
+      const div = document.createElement("div");
+      div.id = "s" + i + j;
+      div.classList.add("squares");
+
+      if (black) {
+        black = false;
+        div.classList.add("black");
+      } else black = true;
+
+      temp.push(div);
+      board.appendChild(div);
+    }
+    squares.push(temp);
+  }
+
+  return { content, board, squares };
 };
 
 const el = elements();
-console.log(el);
-console.log(el.content);
+console.log(el.squares);
 
 /******/ })()
 ;
